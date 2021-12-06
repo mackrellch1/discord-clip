bd374c18431319483df141f1da21730f1e6d6f15
Torrey Leonard <torreyleonard@outlook.com>
2021-12-06 19:53:39 +0000

    Update logger to use console

--- src/logger.ts
+++ src/logger.ts
@@ -2,11 +2,12 @@ import { createLogger, format, transports } from 'winston'
 
 const logConfiguration = {
     transports: [
+        new transports.Console(),
         new transports.File({
             level: 'info',
             filename: 'console.log'
-        })
-    ]
+        }),
+    ],
 };
 
 export const logger = createLogger(logConfiguration);
